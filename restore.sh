5fb384f31cfa219d88a4c0d5c60964a45a557e55
tangduongtrieu <tangduongtrieu@gmail.com>
2017-03-02 16:29:52 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -6,15 +6,13 @@
 	cp -rf /var/backups/$day/* /home/nginx/domains/
 	cp -rf /var/backups/$day/conf/* /usr/local/nginx/conf/
 	cd /home/nginx/domains
-	unzip *.zip
-	gunzip *.sql.gz
+	unzip -o '*.zip'
+	gunzip '*.sql.gz'
 	
-	find . -cdname $day_*.zip -exec rm -rf {} \;
-	find . -cdname $day_*.gzip -exec rm -rf {} \;
+	find . -name "$day*.zip" -exec rm -rf {} \;
 	
 	passdata=`grep -oP "(?<=password=).*" ~/.my.cnf`
 	mysql -u root -p$passdata < *.sql
-	find . -cdname $day_*.zip -exec rm -rf {} \;
-	find . -cdname $day_*.sql -exec rm -rf {} \;
+	find . -name "$day*.sql" -exec rm -rf {} \;
 	
 	service nginx restart

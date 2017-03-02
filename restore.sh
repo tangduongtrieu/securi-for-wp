29544af84fbf478063fa79cdc4a12e291d2a31f7
tangduongtrieu <tangduongtrieu@gmail.com>
2017-03-02 16:12:39 +0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -2,17 +2,19 @@
 	echo "Starting Restore source for All..."
 	day=02-03-2017
 	cd /var/backups/$day
-	find . -name "*.zip" | grep -oP "(?<=.).*/" | while read filename; do mkdir "/var/backups/"$day$filename"log/"; done;
-	yes | cp -rf /var/backups/$day/* /home/nginx/domains/
-	yes | cp -rf /var/backups/$day/conf/* /usr/local/nginx/conf/
-	cd /home/nginx/domains
-	find . -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`" "$filename"; done;
-	find . -cdname *source.zip -exec rm -rf {} \;
 	
-	echo "Starting Restore database for All..."
+	cp -rf /var/backups/$day/* /home/nginx/domains/
+	cp -rf /var/backups/$day/conf/* /usr/local/nginx/conf/
+	cd /home/nginx/domains
+	unzip *.zip
 	gunzip *.sql.gz
+	
+	find . -cdname $day_*.zip -exec rm -rf {} \;
+	find . -cdname $day_*.gzip -exec rm -rf {} \;
+	
 	passdata=`grep -oP "(?<=password=).*" ~/.my.cnf`
 	mysql -u root -p$passdata < *.sql
-	find . -name "*database.sql" -exec rm -rf {} \;
-	find . -name "*database.sql.gz" -exec rm -rf {} \;
+	find . -cdname $day_*.zip -exec rm -rf {} \;
+	find . -cdname $day_*.sql -exec rm -rf {} \;
+	
 	service nginx restart
